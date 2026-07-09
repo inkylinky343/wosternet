acfd9171ec893f65209ac4e2ce8ae08e41fb90cc
inkylinky343 <141261711+inkylinky343@users.noreply.github.com>
2026-07-09 15:11:07 -0500

    bigger and better

--- WOSternet.lua
+++ WOSternet.lua
@@ -1,5 +1,20 @@
+-- open sourcifier or whatever
 local modem = GetPartFromPort(1, "Modem")
-local mc = GetPartFromPort(2, "Microcontroller")
+local mc = GetPartFromPort(1, "Microcontroller")
 local code = modem:GetAsync("https://raw.githubusercontent.com/inkylinky343/wosternet/refs/heads/main/WOSternet.lua", true, nil)
 print(code)
 mc:Configure({Code=code})
+-- borrowed code from stack overflow for generating random strings
+function makeip(k)
+    local alphabet = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789"
+    local n = string.len(alphabet)
+    local pw = {}
+    for i = 1, k
+    do
+        pw[i] = string.byte(alphabet, math.random(n))
+    end
+    return string.char(table.unpack(pw))
+end
+
+local disk = GetPartFromPort(1, Disk)
+local ip = makeip(6)
